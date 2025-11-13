412f700e7d0c7779c742f96f14f122bff3fccba2
Karen Alvarez <kalva088@ucr.edu>
2025-11-13 09:29:40 +0100

    started

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
-def main():
-    print("Hello, world!")
+#imports IMDb data from the dowloaded datasets
+#automatically splits the data into train and testing
+from datasets import load_dataset
+imdb = load_dataset("imdb")
 
-if __name__ == "__main__":
-    main()
+#print(imdb["train"][0])
+
+#example, 
+#imdb["test"][0]
+#{
+#    "label": 0,
+#    "text": "I love sci-fi and am willing to put up with a lot. Sci-fi movies/TV are usually underfunded, under-appreciated and misunderstood. I tried to like this, I really did, but it is to good TV sci-fi as Babylon 5 is to Star Trek (the original). Silly prosthetics, cheap cardboard sets, stilted dialogues, CG that doesn't match the background, and painfully one-dimensional characters cannot be overcome with a 'sci-fi' setting. (I'm sure there are those of you out there who think Babylon 5 is good sci-fi TV. It's not. It's clichéd and uninspiring.) While US viewers might like emotion and character development, sci-fi is a genre that does not take itself seriously (cf. Star Trek). It may treat important issues, yet not as a serious philosophy. It's really difficult to care about the characters here as they are not simply foolish, just missing a spark of life. Their actions and reactions are wooden and predictable, often painful to watch. The makers of Earth KNOW it's rubbish as they have to always say \"Gene Roddenberry's Earth...\" otherwise people would not continue watching. Roddenberry's ashes must be turning in their orbit as this dull, cheap, poorly edited (watching it without advert breaks really brings this home) trudging Trabant of a show lumbers into space. Spoiler. So, kill off a main character. And then bring him back as another actor. Jeeez! Dallas all over again.",
+#}
+
+#transformer, turns text into numbers, doesnt care about caps, numbers represent the text
+from transformers import AutoTokenizer
+tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
+
+#function takes in review and outputs as token, applied to everything in dataset
+def preprocess_function(examples):
+    return tokenizer(examples["text"], truncation=True)
+
+tokenized_imdb = imdb.map(preprocess_function, batched=True)
+
+#preparing for training, pad shortens things so theyre all same length
+from transformers import DataCollatorWithPadding
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+
+
+
+
+
