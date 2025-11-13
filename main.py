07d6b57aef334d5a848235f37bc8583638322818
Karen Alvarez <kalva088@ucr.edu>
2025-11-13 09:45:21 +0100

    train

--- main.py
+++ main.py
@@ -26,8 +26,48 @@ tokenized_imdb = imdb.map(preprocess_function, batched=True)
 from transformers import DataCollatorWithPadding
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
+#will check accuracy of calssification
+import evaluate
+accuracy = evaluate.load("accuracy")
 
+#converts model outputs into readable,convert to real labels, returns accuracy
+import numpy as np
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    predictions = np.argmax(predictions, axis=1)
+    return accuracy.compute(predictions=predictions, references=labels)
 
+#creates label so negative 0 and positive 1
+id2label = {0: "NEGATIVE", 1: "POSITIVE"}
+label2id = {"NEGATIVE": 0, "POSITIVE": 1}
 
+#classifys text into 2 categories and attaches the label mapping to be readable
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+model = AutoModelForSequenceClassification.from_pretrained(
+    "distilbert/distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id
+)
 
-
+#settings for the training
+training_args = TrainingArguments(
+    output_dir="my_awesome_model",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    #push_to_hub=True,
+)
+#does the actual training
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_imdb["train"],
+    eval_dataset=tokenized_imdb["test"],
+    processing_class=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+trainer.train() #trainer
